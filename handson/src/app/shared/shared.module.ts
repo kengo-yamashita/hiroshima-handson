05b7170d8bbd0cfe0ba7ea92f9113ef26fe4b61c
unknown <yamashita-kenngo@outlook.jp>
2019-10-27 13:46:04 +0900

    login form add

--- handson/src/app/shared/shared.module.ts
+++ handson/src/app/shared/shared.module.ts
@@ -6,7 +6,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { MatDialogModule } from '@angular/material/dialog';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [TopMenuComponent, LoginComponent, RegisterComponent],
   imports: [
@@ -14,6 +16,9 @@ import { MatDialogModule } from '@angular/material/dialog';
     MatToolbarModule,
     MatButtonModule,
     MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
   ],
   exports: [
     TopMenuComponent,
